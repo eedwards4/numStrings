354eb72d601270f1706c4276970029833fe9c5ef
Ethan Edwards <evanevansonbusiness@gmail.com>
2024-10-12 19:25:25 -0700

    Maybe sort of working?

--- numStrings.cpp
+++ numStrings.cpp
@@ -2,21 +2,20 @@
 #include <iostream>
 #include <vector>
 #include <chrono> // For timing
+#include <thread>
+#include <mutex>
 #include "mini-gmp/mini-gmp.h"
 
-const std::vector<int> alphabet = {'a', 'b', 'c', 'd'};
-
-// Determine if a state is valid
-bool isValid(std::vector<int>& v){
-    std::vector<bool> found (4, false);
-    for (int i : v){
-        if (i == 1){
+bool contains_all_four(std::vector<int> prev) {
+    bool found[4] = {false, false, false, false};
+    for (int c : prev) {
+        if (c == 1) {
             found[0] = true;
-        } else if (i == 2){
+        } else if (c == 2) {
             found[1] = true;
-        } else if (i == 3){
+        } else if (c == 3) {
             found[2] = true;
-        } else if (i == 4){
+        } else if (c == 4) {
             found[3] = true;
         }
     }
@@ -24,13 +23,35 @@ bool isValid(std::vector<int>& v){
 }
 
 // Main function to recursively calculate number of valid strings of length n
-void numStringsHelper(mpz_t& result, int n, int depth, std::vector<int> prev_5){
+void numStringsHelper(mpz_t& result, int n, int depth, std::vector<int> prev, int next){
+    prev.push_back(next); // Add the next character to the previous characters
 
+    if (depth == n) {
+        if (contains_all_four(prev)) {
+            mpz_add_ui(result, result, 1); // Increment the result by 1
+        }
+    }
+    else {
+        if (depth > 6) { // If the depth is less than 6 we are still building the window
+            if (!contains_all_four(prev)) { // If the window does not contain all four characters, we can return early
+                return;
+            }
+            prev.erase(prev.begin()); // Remove the first character from the window
+        }
+        for (int i = 1; i <= 4; i++) { // Recursively call the function for each possible next character
+            numStringsHelper(result, n, depth + 1, prev, i);
+        }
+    }
 }
 
 // Caller target
 void numStrings(int n, mpz_t& result) {
     mpz_set_ui(result, 0); // Initialize the result to 0
+
+    for (int i = 1; i <= 4; i++) {
+        std::vector<int> prev;
+        numStringsHelper(result, n, 1, prev, i);
+    }
 }
 
 int main() {
